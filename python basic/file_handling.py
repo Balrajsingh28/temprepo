6e4ad048b19743659c1aa349e653dce5ad02f946
Balrajsingh28 <136143883+Balrajsingh28@users.noreply.github.com>
2024-07-04 19:12:51 +0530

    Add files via upload

---
+++ python basic/file_handling.py
@@ -0,0 +1,38 @@
+# f=open ('bca.txt',"w")
+# f.write('hello')
+# print ('write successful')
+# f.close ()
+
+# list1=['balraj \n','rohan\n','pratham']
+# f=open ('bca.txt',"a")
+# f.writelines(list1)
+# print ('write successful')
+# f.close ()
+
+
+# f=open ('bca.txt',"r")
+# line =f.readline()
+# print (line)
+# f.close ()
+
+# f=open ('bca.txt',"r")
+# lines =f.readlines()
+# print (lines)
+#  f.close ()
+
+# f=open ('bca.txt',"r")
+# line=f.read(5)
+# print (line)
+# lines =f.read()
+# print (lines)
+# f.close ()
+
+
+
+# f=open ("bca.txt ","r")
+# print (f.tell())
+# f.read(5)
+# print (f.tell())
+# print(f.seek(7))
+# print (f.readline())
+# print (f.tell())
